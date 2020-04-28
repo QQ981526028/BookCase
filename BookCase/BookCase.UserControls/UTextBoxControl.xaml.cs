763573594d095d6576202888c160d4ea25c18438
AiLiu981526028@outlook.com <AiLiu981526028@outlook.com>
2020-04-28 23:20:47 +0800

    封装输入账号相关用户控件，该控件包含标识是否必填lable，输入框textbox以及消息验证提醒文本框

---
+++ BookCase/BookCase.UserControls/UTextBoxControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookCase.UserControls
+{
+    /// <summary>
+    /// UTextBoxControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class UTextBoxControl : UserControl
+    {
+        public UTextBoxControl()
+        {
+            InitializeComponent();
+            this.Loaded += UTextBoxControl_Loaded;
+        }
+
+        private void UTextBoxControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.lblRedStar.Visibility = RedStarVisibility;
+        }
+        #region 暴露属性
+        /// <summary>
+        /// 红色标记星星是否显示
+        /// </summary>
+        public Visibility RedStarVisibility
+        {
+            get { return (Visibility)GetValue(RedStarVisibilityProperty); }
+            set { SetValue(RedStarVisibilityProperty, value); }
+        }
+        #endregion
+        #region 依赖属性
+        /// <summary>
+        /// 红色标记星星是否显示
+        /// </summary>
+        public static readonly DependencyProperty RedStarVisibilityProperty =
+            DependencyProperty.Register("RedStarVisibility", typeof(Visibility), typeof(UTextBoxControl), new PropertyMetadata(Visibility.Collapsed));
+        #endregion
+
+    }
+}
--- BookCase/BookCase.ViewModel/HomePageViewModel.cs
+++ BookCase/BookCase.ViewModel/HomePageViewModel.cs
@@ -21,12 +21,12 @@ namespace BookCase.ViewModel
         #region 重写父类
         protected override void InitViewModel()
         {
-            
+
         }
 
         protected override void ResetViewModel()
         {
-            
+
         }
         #endregion
         #region Command
@@ -53,28 +53,28 @@ namespace BookCase.ViewModel
         /// </summary>
         private void BorrowBook()
         {
-
+            navigationService.NavigateTo(ViewNames.BORROWBOOK_VIEW);
         }
         /// <summary>
         /// 还书
         /// </summary>
         private void ReturnBook()
         {
-
+            navigationService.NavigateTo(ViewNames.RETURNBOOK_VIEW);
         }
         /// <summary>
         /// 预约
         /// </summary>
         private void ReservationBook()
         {
-
+            navigationService.NavigateTo(ViewNames.RESERVATIONBOOK_VIEW);
         }
         /// <summary>
         /// 登陆
         /// </summary>
         private void Login()
         {
-
+            navigationService.NavigateTo(ViewNames.LOGIN_VIEW);
         }
         #endregion
     }
